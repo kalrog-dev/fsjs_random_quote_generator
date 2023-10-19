4ed222e3a34829e90576c8b8975773c091bbbae4
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-10-19 14:27:16 +0200

    Add quotes and import local fonts
    Use local fonts instead of loading Google Fonts to comply with EU privacy law.

--- js/script.js
+++ js/script.js
@@ -10,8 +10,30 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+const quotes = [
+  {
+    quote: "Not all those who wander are lost.",
+    source: "J. R. R. Tolkien",
+    citation: "The Fellowship of the Ring",
+    year: "1954"
+  },
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    source: "Oscar Wilde"
+  },
+  {
+    quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+    source: "Mahatma Gandhi "
+  },
+  {
+    quote: "It is never too late to be what you might have been.",
+    source: "George Eliot"
+  },
+  {
+    quote: "Everything you can imagine is real.",
+    source: "Pablo Picasso "
+  }
+];
 
 /***
  * `getRandomQuote` function
